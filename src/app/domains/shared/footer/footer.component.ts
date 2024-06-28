60d4bf5373aae5a39cbb2e74ab295df04bbf6f7b
BilbaoJ <jessicabilbao02@gmail.com>
2024-06-28 16:31:11 -0500

    components visibility

--- src/app/domains/shared/footer/footer.component.ts
+++ src/app/domains/shared/footer/footer.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '@shared/services/task.service';
+import { Task } from 'src/app/models/task.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
+  imports: [ CommonModule ],
   standalone: true,
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
 
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(){
+    this.taskService.getTasks().subscribe(tasks => {
+      this.tasks = tasks;
+    });
+  }
 }
