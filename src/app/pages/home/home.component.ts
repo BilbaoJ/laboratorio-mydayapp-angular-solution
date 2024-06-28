87bf2d6ac65e3e77536e4e9aff1e9a55cf58cdf5
BilbaoJ <jessicabilbao02@gmail.com>
2024-06-28 11:58:39 -0500

    Distribution of responsibilities

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '@shared/header/header.component'
+import { FooterComponent } from '@shared/footer/footer.component'
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [ FooterComponent, HeaderComponent, CommonModule  ],
   templateUrl: './home.component.html',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent{
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
